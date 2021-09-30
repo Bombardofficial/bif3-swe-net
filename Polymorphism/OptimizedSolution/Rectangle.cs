6ce7dec064984d7e500a412707fb7b48bc20f1be
Botond Kovacs <if20b109@technikum-wien.at>
2021-09-30 15:24:36 +0200

    Rectangle finished

---
+++ Polymorphism/OptimizedSolution/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Bif3.Swe1.Oop.Polymorphism.OptimizedSolution
+{
+    public class Rectangle : Shape, IShapeComposition
+    {
+        private int _sideA, _sideB;
+        private int factor = 2;
+        public Rectangle(int x, int y, int a, int b) : base(x, y)
+        {
+            _sideA = a;
+            _sideB = b;
+        }
+
+        public double getArea()
+        {
+            return _sideA * _sideB;
+        }
+
+        public double GetPerimeter()
+        {
+            return (factor * _sideA) + (factor * _sideB);
+        }
+        public voide PrintShapeType()
+        {
+            Console.WriteLine("I'm a Rectangle.")
+        }
+    }
+
+
+
+
+}
